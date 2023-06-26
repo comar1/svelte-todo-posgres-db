f10c9bff4b251996055bbb0eb4b7ed15c2c2935b
comar1 <officialfranciscomar@gmail.com>
2023-06-27 00:56:39 +0800

    first commit, no api, db

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  let todos = [{ done: false, message: "do dishes" }];
+
+  function add() {
+    todos = todos.concat({ done: false, message: "" });
+  }
+
+  function clear() {
+    todos = todos.filter((t) => !t.done);
+  }
+
+  $: remaining = todos.filter((t) => !t.done).length;
+</script>
+
+<h2>Todo List</h2>
+{#each todos as todo}
+  <div class:done={todo.done}>
+    <input type="checkbox" bind:checked={todo.done} />
+    <input
+      type="textbox"
+      placeholder="what needs to be done?"
+      bind:value={todo.text}
+    />
+  </div>
+{/each}
+
+<button on:click={add}>Add new</button>
+<button on:click={clear} disabled>Clear</button>
+
+<p>{remaining} remaining</p>
+
+<style>
+  .done {
+    opacity: 0.4;
+  }
+</style>
